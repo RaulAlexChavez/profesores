af1afc7db16876de09ac548947871086ba1bc23f
RaulAlexChavez <chavezra.personal@gmail.com>
2022-10-02 22:16:22 -0700

    I think I did a build or smth

--- profesores/entidad/target/generated-sources/annotations/mx/alesco/entidad/Profesor_.java
+++ profesores/entidad/target/generated-sources/annotations/mx/alesco/entidad/Profesor_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 import mx.alesco.entidad.UnidadDeAprendizaje;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2022-10-01T17:25:11")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2022-10-01T20:33:46")
 @StaticMetamodel(Profesor.class)
 public class Profesor_ { 
 
